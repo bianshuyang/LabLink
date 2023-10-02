65051bb71c4dd898a03fb80137c9eb45ba1bfcb9
Shuyang Bian <simon.bian@emory.edu>
2023-10-02 17:15:33 -0400

    Make a minimal input and submit type

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 
+import { Link } from "react-router-dom"
 
-
+import postsData from './forum_data.json';
 // Mock data
 const usersData = [
     { netid: 'abc01', Name: 'Alice', address: 'alice@example.com' },
     { netid: 'def02', Name: 'Bob', address: 'bob@example.com' }
 ];
 
-const postsData = [
-    { netid: 'abc01', posttitle: 'Post by Alice', postcontent: 'This is a post by Alice.', postdate: '2023-10-01', postid: 1 },
-    { netid: 'def02', posttitle: 'Post by Bob', postcontent: 'This is a post by Bob.', postdate: '2023-10-02', postid: 2 }
-];
+
 
 const repliesData = [
     { netid: 'def02', replycontent: 'Reply by Bob to Alice', replydate: '2023-10-03', replyid: 'uuid-1', postid: 1 },
@@ -40,21 +38,109 @@ function Forum() {
 
     // Depending on the state, we'll render different views
     return (
-        <>
+       
+
+    <>
+
+<div className="site-mobile-menu">
+        <div className="site-mobile-menu-header">
+          <div className="site-mobile-menu-close">
+            <span className="icofont-close js-menu-toggle"></span>
+          </div>
+        </div>
+        <div className="site-mobile-menu-body"></div>
+      </div>
+
+      <nav className="site-nav mb-5">
+        <div className="pb-2 top-bar mb-3">
+          <div className="container">
+            <div className="row align-items-center">
+
+              <div className="col-6 col-lg-9">
+                <Link to={'/'} className="small mr-3"><span className="icon-question-circle-o mr-2"></span> <span className="d-none d-lg-inline-block">Contact us</span></Link>
+                <Link to={'/'} className="small mr-3"><span className="icon-phone mr-2"></span> <span className="d-none d-lg-inline-block"></span></Link>
+                <Link to={'/'} className="small mr-3"><span className="icon-envelope mr-2"></span> <span className="d-none d-lg-inline-block"></span></Link>
+              </div>
+
+              <div className="col-6 col-lg-3 text-right">
+                <Link to={'/login'} className="small mr-3">
+                  <span className="icon-lock"></span>
+                  Log In
+                </Link>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="sticky-nav js-sticky-header">
+          <div className="container position-relative">
+            <div className="site-navigation text-center">
+              <Link to={'/'} className="logo menu-absolute m-0">LabLink!<span className="text-primary">.</span></Link>
+
+              <ul className="js-clone-nav d-none d-lg-inline-block site-menu">
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/'}>Professors</Link></li>
+              <li><Link to={'/news'}>Projects</Link></li>
+              <li><Link to={'/news'}>Events</Link></li>
+              <li><Link to={'/'}>About</Link></li>
+              <li><Link to={'/'}>Contact</Link></li>
+              <li  className="active" ><Link to={'/forum'}>Forum</Link></li>
+            </ul>
+
+              <Link to={'/'} className="btn-book btn btn-secondary btn-sm menu-absolute">Enroll Now</Link>
+
+              <Link to={'/'} className="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light" data-toggle="collapse" data-target="#main-navbar">
+                <span></span>
+              </Link>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="untree_co-hero overlay">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-12">
+              <div className="row justify-content-center ">
+                <div className="col-lg-6 text-center ">
+                  <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Forum of Discussion</h1>
+                  <div className="mb-5 text-white desc mx-auto" data-aos="fade-up" data-aos-delay="200">
+                    <p>A great place for discussion. </p>
+                    <p>All @ Emory.</p>
+                  </div>
+
+                  <p className="mb-0" data-aos="fade-up" data-aos-delay="300">
+        <a href="#News_concrete" className="btn btn-secondary">Check on the forum</a>
+    </p>
 
 
-{/* Dropdown menu to select view */}
+                </div>
+
+
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className="untree_co-section bg-light" id = "News_concrete">
+        <div className="container">
+
+
+
             <div>
                 <label>Select View: </label>
                 <select value={view} onChange={handleViewChange}>
-                    <option value="users">Users</option>
                     <option value="posts">Posts</option>
                     <option value="replies">Replies</option>
                 </select>
             </div>
 
 
-            {/* Main Content */}
             <main>
                 
 
@@ -64,7 +150,7 @@ function Forum() {
                         <ul>
                             {postsData.map(user => (
                                 <li key={user.netid} onClick={() => handleUserClick(user.netid)}>
-                                    <strong>{user.posttitle}</strong> ({user.postcontent}) - {user.postdate}
+                                    <strong>{user.posttitle}</strong> ({user.description}) - {user.postdate}
                                 </li>
                             ))}
                         </ul>
@@ -88,7 +174,95 @@ function Forum() {
                 )}
 
             </main>
-        </>
+
+  </div>
+
+
+<div className="site-footer">
+
+
+        <div className="container">
+
+          <div className="row">
+            <div className="col-lg-3 mr-auto">
+              <div className="widget">
+                <h3>About Us<span className="text-primary">.</span> </h3>
+                <p></p>
+              </div>
+              <div className="widget">
+                <h3>Connect</h3>
+                <ul className="list-unstyled social">
+                  <li><Link to={'/'}><span className="icon-instagram"></span></Link></li>
+                  <li><Link to={'/'}><span className="icon-twitter"></span></Link></li>
+                  <li><Link to={'/'}><span className="icon-facebook"></span></Link></li>
+                  <li><Link to={'/'}><span className="icon-linkedin"></span></Link></li>
+                  <li><Link to={'/'}><span className="icon-pinterest"></span></Link></li>
+                  <li><Link to={'/'}><span className="icon-dribbble"></span></Link></li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-2 ml-auto">
+              <div className="widget">
+                <h3>Projects</h3>
+                <ul className="list-unstyled float-left links">
+                  <li><Link to={'/'}>Web Design</Link></li>
+                  <li><Link to={'/'}>HTML5</Link></li>
+                  <li><Link to={'/'}>CSS3</Link></li>
+                  <li><Link to={'/'}>jQuery</Link></li>
+                  <li><Link to={'/'}>Bootstrap</Link></li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-3">
+              <div className="widget">
+                <h3>Gallery</h3>
+                <ul className="instafeed instagram-gallery list-unstyled">
+                  <li><a className="instagram-item" href="images/gal_1.jpg" data-fancybox="gal"><img src="images/gal_1.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                  <li><a className="instagram-item" href="images/gal_2.jpg" data-fancybox="gal"><img src="images/gal_2.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                  <li><a className="instagram-item" href="images/gal_3.jpg" data-fancybox="gal"><img src="images/gal_3.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                  <li><a className="instagram-item" href="images/gal_4.jpg" data-fancybox="gal"><img src="images/gal_4.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                  <li><a className="instagram-item" href="images/gal_5.jpg" data-fancybox="gal"><img src="images/gal_5.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                  <li><a className="instagram-item" href="images/gal_6.jpg" data-fancybox="gal"><img src="images/gal_6.jpg" alt="" width="72" height="72"/></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
+            <div className="col-lg-3">
+            <div className="widget">
+              <h3>Contact</h3>
+              <address>201 Dowman Dr, Atlanta, GA 30322</address>
+              <ul className="list-unstyled links mb-4">
+                <li><a href="tel://4047276123">+1(404) 727-6123</a></li>
+                <li><a href="email://jeff.epstein@emory.edu">jeff.epstein@emory.edu</a></li>
+              </ul>
+            </div>
+          </div>
+
+          </div>
+
+          <div className="row mt-5">
+          <div className="col-12 text-center">
+            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+        </div>
+
+</div>
+
+
+
+</>
+
     );
 }
 
---
+++ src/components/Register.js
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+import eLogo from "../images/eLogo.png";
+
+function Register(){
+
+  const [isChange, setIsChange] = React.useState(false);
+
+  const [netID, setNetID] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleButtonClick = () => {
+    setIsChange(!isChange);
+  };
+
+  const handleFormSubmit = (e) => {
+      console.log("NetID:", netID);
+      console.log("Password:", password);
+      e.preventDefault();
+      // Here, you can now use the netID and password state variables
+      // to send to your server or perform other operations.
+
+  };
+  return(
+      <div className={`login-container ${isChange ? "change" : ""}`}>
+        <div className="login-form-wrapper">
+          <div className="eLogo">
+            <img src={eLogo} alt="Emory Logo"/>
+          </div>
+          <div className="banner">
+            <h1>Lab Link</h1>
+            <p>Enter your Emory credential and start journey with us!</p>
+          </div>
+          <div className="blue-bg">
+            <div className={`logo-2 ${isChange ? "change" : ""}`} id="logoonce">
+              <img src={eLogo} alt="Emory Logo"/>
+            </div>
+            <button type="button" onClick={() => handleButtonClick()}>Lab Link</button>
+          </div>
+          <form className="signin-form" onSubmit={handleFormSubmit}>
+            <h1>Lab Link</h1>
+            <h1>Register</h1>
+            <p>Register as a guest to collect your favorites or cast votes!</p>
+            <div className="input-group">
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="NetID" value = {netID} onChange={e => setNetID(e.target.value)}  />
+            </div>
+            <div className="input-group">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" value={password}  onChange={e => setPassword(e.target.value)}  />
+            </div>
+            <button type="submit">Register!</button>
+          </form>
+        </div>
+      </div>
+  )
+}
+
+export default Register
