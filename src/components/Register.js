889a749c0828de1f94a59bede46f02b3a13841ac
Shuyang Bian <simon.bian@emory.edu>
2023-10-05 18:23:42 -0400

    pushing a minimal viable instance of mongo db connect

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,61 +1,53 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
-import "../styles/login.css";
-import eLogo from "../images/eLogo.png";
+// src/Components/DataFetcher.js
 
-function Register(){
+import { useEffect, useState } from "react";
 
-  const [isChange, setIsChange] = React.useState(false);
+function Register() {
+  const [data, setData] = useState(null);  // Use null as an initial state to signify 'not yet loaded'
+  const [loading, setLoading] = useState(true);
 
-  const [netID, setNetID] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  async function fetchData() {
+  try {
+    const response = await fetch("http://localhost:3000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        field1: "1",
+        field2: "1",
+      }),
+    });
 
-  const handleButtonClick = () => {
-    setIsChange(!isChange);
-  };
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-  const handleFormSubmit = (e) => {
-      console.log("NetID:", netID);
-      console.log("Password:", password);
-      e.preventDefault();
-      // Here, you can now use the netID and password state variables
-      // to send to your server or perform other operations.
-
-  };
-  return(
-      <div className={`login-container ${isChange ? "change" : ""}`}>
-        <div className="login-form-wrapper">
-          <div className="eLogo">
-            <img src={eLogo} alt="Emory Logo"/>
-          </div>
-          <div className="banner">
-            <h1>Lab Link</h1>
-            <p>Enter your Emory credential and start journey with us!</p>
-          </div>
-          <div className="blue-bg">
-            <div className={`logo-2 ${isChange ? "change" : ""}`} id="logoonce">
-              <img src={eLogo} alt="Emory Logo"/>
-            </div>
-            <button type="button" onClick={() => handleButtonClick()}>Lab Link</button>
-          </div>
-          <form className="signin-form" onSubmit={handleFormSubmit}>
-            <h1>Lab Link</h1>
-            <h1>Register</h1>
-            <p>Register as a guest to collect your favorites or cast votes!</p>
-            <div className="input-group">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="NetID" value = {netID} onChange={e => setNetID(e.target.value)}  />
-            </div>
-            <div className="input-group">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" value={password}  onChange={e => setPassword(e.target.value)}  />
-            </div>
-            <button type="submit">Register!</button>
-            <Link to={'/'}><button type="submit">Go to the Home Page</button></Link>
-          </form>
-        </div>
-      </div>
-  )
+    const responseData = await response.json();
+    setData(responseData);
+    setLoading(false);
+    console.log("Done loading!");
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    setLoading(false);
+  }
 }
 
-export default Register
+
+  useEffect(() => {
+    fetchData();  // Call the fetchData function when the component mounts
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;  // Display loading indicator when data is being fetched
+  }
+
+  return (
+    <div>
+      <h1>Data from MongoDB</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default Register;
