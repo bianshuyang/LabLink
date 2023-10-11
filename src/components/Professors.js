bc6db5d2e2d530e6e409e6414aab0ef2f8cbd4e7
scui24 <123330983+scui24@users.noreply.github.com>
2023-10-11 15:51:04 -0400

    Update Professors.js

--- src/components/Professors.js
+++ src/components/Professors.js
@@ -5,23 +5,6 @@ import { Modal, Button } from 'react-bootstrap';
 const ITEMS_PER_PAGE = 10;
 const MAX_VISIBLE_PAGINATION = 8;
 
-// function SampleComponent() {
-//   const [count, setCount] = useState(0);
-
-//   const incrementCount = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Welcome to SampleComponent</h1>
-//       <p>Count: {count}</p>
-//       <button onClick={incrementCount}>Increment Count</button>
-//     </div>
-//   );
-
-// }
-
 function SampleComponent() {
   return (
     <div>
@@ -65,15 +48,16 @@ function Professors() {
     <>
 
 <div className="site-mobile-menu">
-        <div className="site-mobile-menu-header">
-          <div className="site-mobile-menu-close">
-            <span className="icofont-close js-menu-toggle"></span>
-          </div>
-        </div>
-        <div className="site-mobile-menu-body"></div>
-      </div>
+  <div className="site-mobile-menu-header">
+    <div className="site-mobile-menu-close">
+      <span className="icofont-close js-menu-toggle"></span>
+    </div>
+  </div>
+  
+  <div className="site-mobile-menu-body"></div>
+  </div>
 
-      <nav className="site-nav mb-5">
+  <nav className="site-nav mb-5">
         <div className="pb-2 top-bar mb-3">
           <div className="container">
             <div className="row align-items-center">
@@ -119,85 +103,73 @@ function Professors() {
             </div>
           </div>
         </div>
-      </nav>
+  </nav>
 
-      <div className="untree_co-hero overlay">
-        <div className="container">
-          <div className="row align-items-center justify-content-center">
-            <div className="col-12">
-              <div className="row justify-content-center ">
-                <div className="col-lg-6 text-center ">
-                  <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Professors</h1>
-                  <div className="mb-5 text-white desc mx-auto" data-aos="fade-up" data-aos-delay="200">
-                    <p>Professors. </p>
-                    <p>All @ Emory.</p>
-                  </div>
-
-                  <p className="mb-0" data-aos="fade-up" data-aos-delay="300">
-        <a href="#News_concrete" className="btn btn-secondary">Explore instructors</a>
-    </p>
-
-
-                </div>
-
-
-
+  <div className="untree_co-hero overlay">
+    <div className="container">
+      <div className="row align-items-center justify-content-center">
+        <div className="col-12">
+          <div className="row justify-content-center ">
+            <div className="col-lg-6 text-center ">
+              <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Professors</h1>
+              <div className="mb-5 text-white desc mx-auto" data-aos="fade-up" data-aos-delay="200">
+                <p>Professors. </p>
+                <p>All @ Emory.</p>
               </div>
-
+              <p className="mb-0" data-aos="fade-up" data-aos-delay="300">
+                <a href="#News_concrete" className="btn btn-secondary">Explore instructors</a>
+              </p>
             </div>
-
           </div>
         </div>
       </div>
-      <div className="untree_co-section bg-light" id = "News_concrete">
-        <div className="container">
-
-            
-
+    </div>
   </div>
-
-
-<div className="site-footer">
-
-
-        <div className="container">
-
-          <div className="row">
-            <div className="col-lg-3 mr-auto">
-              <div className="widget">
-                <h3>About Us<span className="text-primary">.</span> </h3>
-                <p></p>
-              </div>
-              <div className="widget">
-                <h3>Connect</h3>
-                <ul className="list-unstyled social">
+      
+  <div className="untree_co-section bg-light" id = "News_concrete">
+    <div className="container">
+    </div>
+  
+  <div className="site-footer">
+    <div className="container">
+      <div className="row">
+        
+        <div className="col-lg-3 mr-auto">
+          <div className="widget">
+            <h3>About Us<span className="text-primary">.</span> </h3>
+              <p></p>
+          </div>
+          
+          <div className="widget">
+            <h3>Connect</h3>
+            <ul className="list-unstyled social">
                   <li><Link to={'/'}><span className="icon-instagram"></span></Link></li>
                   <li><Link to={'/'}><span className="icon-twitter"></span></Link></li>
                   <li><Link to={'/'}><span className="icon-facebook"></span></Link></li>
                   <li><Link to={'/'}><span className="icon-linkedin"></span></Link></li>
                   <li><Link to={'/'}><span className="icon-pinterest"></span></Link></li>
                   <li><Link to={'/'}><span className="icon-dribbble"></span></Link></li>
-                </ul>
-              </div>
-            </div>
+            </ul>
+          </div>
+        </div>
 
-            <div className="col-lg-2 ml-auto">
-              <div className="widget">
-                <h3>Projects</h3>
-                <ul className="list-unstyled float-left links">
+        <div className="col-lg-2 ml-auto">
+          <div className="widget">
+            <h3>Projects</h3>
+            <ul className="list-unstyled float-left links">
                   <li><Link to={'/'}>Web Design</Link></li>
                   <li><Link to={'/'}>HTML5</Link></li>
                   <li><Link to={'/'}>CSS3</Link></li>
                   <li><Link to={'/'}>jQuery</Link></li>
                   <li><Link to={'/'}>Bootstrap</Link></li>
-                </ul>
-              </div>
-            </div>
+            </ul>
+          </div>
+        </div>
 
-            <div className="col-lg-3">
-              <div className="widget">
-                <h3>Gallery</h3>
-                <ul className="instafeed instagram-gallery list-unstyled">
+        <div className="col-lg-3">
+          <div className="widget">
+            <h3>Gallery</h3>
+            <ul className="instafeed instagram-gallery list-unstyled">
                   <li><a className="instagram-item" href="images/gal_1.jpg" data-fancybox="gal"><img src="images/gal_1.jpg" alt="" width="72" height="72"/></a>
                   </li>
                   <li><a className="instagram-item" href="images/gal_2.jpg" data-fancybox="gal"><img src="images/gal_2.jpg" alt="" width="72" height="72"/></a>
@@ -210,39 +182,31 @@ function Professors() {
                   </li>
                   <li><a className="instagram-item" href="images/gal_6.jpg" data-fancybox="gal"><img src="images/gal_6.jpg" alt="" width="72" height="72"/></a>
                   </li>
-                </ul>
-              </div>
-            </div>
+            </ul>
+          </div>
+        </div>
 
 
-            <div className="col-lg-3">
-            <div className="widget">
-              <h3>Contact</h3>
-              <address>201 Dowman Dr, Atlanta, GA 30322</address>
-              <ul className="list-unstyled links mb-4">
+        <div className="col-lg-3">
+          <div className="widget">
+            <h3>Contact</h3>
+            <address>201 Dowman Dr, Atlanta, GA 30322</address>
+            <ul className="list-unstyled links mb-4">
                 <li><a href="tel://4047276123">+1(404) 727-6123</a></li>
                 <li><a href="email://jeff.epstein@emory.edu">jeff.epstein@emory.edu</a></li>
-              </ul>
-            </div>
-          </div>
-
-          </div>
-
-          <div className="row mt-5">
-          <div className="col-12 text-center">
-            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.</p>
-            </div>
+            </ul>
           </div>
         </div>
+
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-12 text-center">
+          <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.</p>
         </div>
-
-// </div>
-
-
-
+      </div>
+    </div>
+  </div>
 </>
-
-    );
-}
 
 export default Professors;
