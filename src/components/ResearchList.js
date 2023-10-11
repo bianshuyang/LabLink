c0306be42f4ce6253410979c2e0e98d696968eb7
scui24 <123330983+scui24@users.noreply.github.com>
2023-10-11 16:54:45 -0400

    Update ProfeesorInfo

--- src/components/ResearchList.js
+++ src/components/ResearchList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function ResearchList() {
-  const courses = ['Computer Science 101', 'Web Development 202', 'Data Structures 301'];
+  const research = ['Research1', 'Research2'];
 
   return (
-    <div className="courses-list">
-      <h2>Courses Taught</h2>
+    <div className="research-list">
+      <h2>Ongoing Research</h2>
       <ul>
-        {courses.map((course, index) => (
-          <li key={index}>{course}</li>
+        {research.map((research, index) => (
+          <li key={index}>{research}</li>
         ))}
       </ul>
     </div>
