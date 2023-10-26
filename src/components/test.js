16d43dc820e65ed2e41cf87a5b221b1e58708d08
Shuyang Bian <simon.bian@emory.edu>
2023-10-26 13:58:18 -0400

    A minimal file of sending emails

--- src/components/test.js
+++ src/components/test.js
@@ -1,24 +1,76 @@
+import React, { useState } from 'react';
 
-import React from 'react';
-import { useNavigate } from "react-router-dom";
- 
-function Test() {
- 
-  const navigate = useNavigate();
- 
-  const handleSaveToken = () => {
-    // Here, we're setting a token in sessionStorage.
-    sessionStorage.setItem('myToken', '123456abcdef');
-    // Navigate to the TokenPage component.
-    navigate('/test2');
+const EmailForm = () => {
+  // State to keep track of form data
+  const [formData, setFormData] = useState({
+    from: "lablnk_help@outlook.com",
+    to: '',
+    subject: 'Lab Link Password Reset',
+    html: 'Dear user, your verification code is: 534 781. Thank you for your interest in lablink!',
+  });
+
+  // State to handle response message
+  const [response, setResponse] = useState('');
+
+  // Handle form inputs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
- 
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send a POST request to the serverless function
+      const res = await fetch('/api/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      // Error handling with the response
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // Update state with the success response (can be customized based on your backend response)
+      setResponse('Email sent successfully!');
+
+      // Optionally, clear the form after successful submission
+      setFormData({
+        from: "lablnk_help@outlook.com",
+        to: '',
+        subject: 'Lab Link Password Reset',
+        html: 'Dear user, your verification code is: 534 781. Thank you for your interest in lablink!',
+      });
+    } catch (error) {
+      // Update state with error message
+      setResponse(`There was an error sending the email: ${error.message}`);
+    }
+  };
+
+  // Render the form
   return (
     <div>
-      <h2>Home</h2>
-      <button onClick={handleSaveToken}>Save Token and Go to Token Page</button>
+      <h3>Contact Us</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="to"
+          placeholder="Recipient's email"
+          value={formData.to}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Send Email</button>
+      </form>
+      {response && <p>{response}</p>} {/* Display the response message if available */}
     </div>
   );
-}
+};
 
-export default Test;
+export default EmailForm;
