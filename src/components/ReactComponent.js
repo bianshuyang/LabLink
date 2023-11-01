9c26d45102f2368a3da05845817c2dac30650ba7
scui24 <123330983+scui24@users.noreply.github.com>
2023-11-01 13:47:12 -0400

    Update ReactComponent.js
    -

--- src/components/ReactComponent.js
+++ src/components/ReactComponent.js
@@ -6,14 +6,14 @@ const ReactComponent = (props) => {
     <div class="container">
       <div class="untree_co-section bg-light">
         <div class="row">
-          {props.data.map((item, index) => (
-            <div class="col-12 col-sm-6 col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay={(index) * 1000}>
+          {props.data.map((prof, index) => (
+            <div class="col-12 col-sm-6 col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay={(index) * 100}>
               <div class="staff text-center">
                 <div class="mb-4">< img src="images/staff_1.jpg" alt="Image" class="img-fluid" /></div>
                 <div class="staff-body">
-                  <h3 class="staff-name">{item.name}</h3>
+                  <h3 class="staff-name">{prof.name}</h3>
                   <span class="d-block position mb-4">Teacher in CS</span>
-                  <p class="mb-5">{item.researchInterest}</p >
+                  <p class="mb-5">{prof.researchInterest}</p >
                 </div>
               </div>
             </div>       
