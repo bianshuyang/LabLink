77a4cd12525ad87932f6e51e473c73b0a9430443
Marcus Hill <hillmarcus32@gmail.com>
2023-11-15 11:01:55 -0500

    Profile Page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,39 +1,309 @@
 import React from 'react';
 import Navbar from './Navbar.js';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import FormHelperText from '@mui/joy/FormHelperText';
+import Input from '@mui/joy/Input';
+import IconButton from '@mui/joy/IconButton';
+import Textarea from '@mui/joy/Textarea';
+import Stack from '@mui/joy/Stack';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Typography from '@mui/joy/Typography';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab, { tabClasses } from '@mui/joy/Tab';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+import Card from '@mui/joy/Card';
+import CardActions from '@mui/joy/CardActions';
+import CardOverflow from '@mui/joy/CardOverflow';
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import AccessTimeFilledRoundedIcon from '@mui/icons-material/AccessTimeFilledRounded';
+import VideocamRoundedIcon from '@mui/icons-material/VideocamRounded';
+import InsertDriveFileRoundedIcon from '@mui/icons-material/InsertDriveFileRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+
 import { LabLinkContext } from '../LabLinkProvider';
 
 export default function Profile() {
 
   const { netID, setNetID } = React.useContext(LabLinkContext);
 
-    return (
-      <>
-        <div className="site-mobile-menu">
-          <div className="site-mobile-menu-header">
-            <div className="site-mobile-menu-close">
-              <span className="icofont-close js-menu-toggle"></span>
-            </div>
-          </div>
-          <div className="site-mobile-menu-body"></div>
+  return (
+    <>
+    <div className="site-mobile-menu">
+      <div className="site-mobile-menu-header">
+        <div className="site-mobile-menu-close">
+          <span className="icofont-close js-menu-toggle"></span>
         </div>
+      </div>
+      <div className="site-mobile-menu-body"></div>
+    </div>
 
-        <Navbar/>
+    <Navbar/>
 
-        <div className="untree_co-hero overlay">
-          <div className="container">
-            <div className="row align-items-center justify-content-center">
-              <div className="col-12">
-                <div className="row justify-content-center ">
-                  <div className="col-lg-6 text-center ">
-                    <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Welcome { netID }</h1>
-                    <div className="mb-5 text-white desc mx-auto" data-aos="fade-up" data-aos-delay="200">
-                      <p>Welcome to the LabLink Profile Page! Here, you have the power to personalize your scientific identity. Update your email, fine-tune your display name, and enhance your professional bio.</p>
-                    </div>
-                  </div>
+    <div className="untree_co-hero overlay">
+      <div className="container">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-12">
+            <div className="row justify-content-center ">
+              <div className="col-lg-6 text-center ">
+                <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Welcome { netID }</h1>
+                <div className="mb-5 text-white desc mx-auto" data-aos="fade-up" data-aos-delay="200">
+                  <p>Welcome to the LabLink Profile Page! Here, you have the power to personalize your scientific identity. Update your email, fine-tune your display name, and enhance your professional bio.</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
-)}
+      </div>
+    </div>
+    <Box
+      sx={{
+        flex: 1,
+        width: '100%',
+      }}
+    >
+      <Stack
+        spacing={4}
+        sx={{
+          display: 'flex',
+          maxWidth: '800px',
+          mx: 'auto',
+          px: {
+            xs: 2,
+            md: 6,
+          },
+          py: {
+            xs: 2,
+            md: 3,
+          },
+        }}
+      >
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Personal info</Typography>
+            <Typography level="body-sm">
+              Customize how your profile information will apper to the networks.
+            </Typography>
+          </Box>
+          <Divider />
+          <Stack
+            direction="row"
+            spacing={3}
+            sx={{ display: { xs: 'none', md: 'flex' }, my: 1 }}
+          >
+            <Stack direction="column" spacing={1}>
+              <AspectRatio
+                ratio="1"
+                maxHeight={200}
+                sx={{ flex: 1, minWidth: 120, borderRadius: '100%' }}
+              >
+                <img
+                  src=""
+                  srcSet=""
+                  loading="lazy"
+                  alt=""
+                />
+              </AspectRatio>
+              <IconButton
+                aria-label="upload new picture"
+                size="sm"
+                variant="outlined"
+                color="neutral"
+                sx={{
+                  bgcolor: 'background.body',
+                  position: 'absolute',
+                  zIndex: 2,
+                  borderRadius: '50%',
+                  left: 100,
+                  top: 170,
+                  boxShadow: 'sm',
+                }}
+              >
+                <EditRoundedIcon />
+              </IconButton>
+            </Stack>
+            <Stack spacing={2} sx={{ flexGrow: 1 }}>
+              <Stack spacing={1}>
+                <FormLabel>Name</FormLabel>
+                <FormControl
+                  sx={{
+                    display: {
+                      sm: 'flex-column',
+                      md: 'flex-row',
+                    },
+                    gap: 2,
+                  }}
+                >
+                  <Input size="sm" placeholder="First name" />
+                  <Input size="sm" placeholder="Last name" sx={{ flexGrow: 1 }} />
+                </FormControl>
+              </Stack>
+              <Stack direction="row" spacing={2}>
+                <FormControl>
+                  <FormLabel>Role</FormLabel>
+                  <Input size="sm" defaultValue="UI Developer" />
+                </FormControl>
+                <FormControl sx={{ flexGrow: 1 }}>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    size="sm"
+                    type="email"
+                    startDecorator={<EmailRoundedIcon />}
+                    placeholder="email"
+                    defaultValue="email@email.com"
+                    sx={{ flexGrow: 1 }}
+                  />
+                </FormControl>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{ display: { xs: 'flex', md: 'none' }, my: 1 }}
+          >
+            <Stack direction="row" spacing={2}>
+              <Stack direction="column" spacing={1}>
+                <AspectRatio
+                  ratio="1"
+                  maxHeight={108}
+                  sx={{ flex: 1, minWidth: 108, borderRadius: '100%' }}
+                >
+                  <img
+                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                    srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                    loading="lazy"
+                    alt=""
+                  />
+                </AspectRatio>
+                <IconButton
+                  aria-label="upload new picture"
+                  size="sm"
+                  variant="outlined"
+                  color="neutral"
+                  sx={{
+                    bgcolor: 'background.body',
+                    position: 'absolute',
+                    zIndex: 2,
+                    borderRadius: '50%',
+                    left: 85,
+                    top: 180,
+                    boxShadow: 'sm',
+                  }}
+                >
+                  <EditRoundedIcon />
+                </IconButton>
+              </Stack>
+              <Stack spacing={1} sx={{ flexGrow: 1 }}>
+                <FormLabel>Name</FormLabel>
+                <FormControl
+                  sx={{
+                    display: {
+                      sm: 'flex-column',
+                      md: 'flex-row',
+                    },
+                    gap: 2,
+                  }}
+                >
+                  <Input size="sm" placeholder="First name" />
+                  <Input size="sm" placeholder="Last name" />
+                </FormControl>
+              </Stack>
+            </Stack>
+
+            <FormControl>
+              <FormLabel>Role</FormLabel>
+              <Input size="sm" defaultValue="UI Developer" />
+            </FormControl>
+            <FormControl sx={{ flexGrow: 1 }}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                size="sm"
+                type="email"
+                startDecorator={<EmailRoundedIcon />}
+                placeholder="email"
+                defaultValue="siriwatk@test.com"
+                sx={{ flexGrow: 1 }}
+              />
+            </FormControl>
+            <div>
+              <FormControl sx={{ display: { sm: 'contents' } }}>
+                <FormLabel>Timezone</FormLabel>
+                <Select
+                  size="sm"
+                  startDecorator={<AccessTimeFilledRoundedIcon />}
+                  defaultValue="1"
+                >
+                  <Option value="1">
+                    Indochina Time (Bangkok){' '}
+                    <Typography textColor="text.tertiary" ml={0.5}>
+                      — GMT+07:00
+                    </Typography>
+                  </Option>
+                  <Option value="2">
+                    Indochina Time (Ho Chi Minh City){' '}
+                    <Typography textColor="text.tertiary" ml={0.5}>
+                      — GMT+07:00
+                    </Typography>
+                  </Option>
+                </Select>
+              </FormControl>
+            </div>
+          </Stack>
+          <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
+            <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
+              </Button>
+              <Button size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Bio</Typography>
+            <Typography level="body-sm">
+              Write a short introduction to be displayed on your profile
+            </Typography>
+          </Box>
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}>
+            <Textarea
+              size="sm"
+              minRows={4}
+              sx={{ mt: 1.5 }}
+              placeholder="Type your bio here..."
+              defaultValue=""
+            />
+            <FormHelperText sx={{ mt: 0.75, fontSize: 'xs' }}>
+              275 characters left
+            </FormHelperText>
+          </Stack>
+          <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
+            <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
+              </Button>
+              <Button size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+      </Stack>
+    </Box>
+    </>
+  );
+}
