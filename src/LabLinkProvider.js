cb45ba9ef57944057186ee543f072f8c12d4f455
Marcus Hill <hillmarcus32@gmail.com>
2023-11-22 17:54:36 -0500

    Auth persistence

--- src/LabLinkProvider.js
+++ src/LabLinkProvider.js
@@ -3,8 +3,11 @@ import * as React from 'react';
 export const LabLinkContext = React.createContext({})
 
 export function LabLinkProvider({ children }){
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [netID, setNetID] = React.useState('');
+
+  const userID = sessionStorage.getItem('userID');
+  const userToken = sessionStorage.getItem('userToken');
+  const [isLoggedIn, setIsLoggedIn] = React.useState(!!userToken);
+  const [netID, setNetID] = React.useState(userID ? userID : '');
   const [name, setName] = React.useState('');
   const [role, setRole] = React.useState('');
   const [email, setEmail] = React.useState('');
@@ -12,6 +15,15 @@ export function LabLinkProvider({ children }){
   const [year, setYear] = React.useState();
   const [major, setMajor] = React.useState('');
   const [courses, setCourses] = React.useState('');
+
+  React.useEffect(() => {
+    setIsLoggedIn(!!userToken);
+  }, [userToken]);
+
+  React.useEffect(() => {
+    setIsLoggedIn(!!userID);
+  }, [userID]);
+
 
   return(
     <LabLinkContext.Provider value={{
