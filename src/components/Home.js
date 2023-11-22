ea91f1944f1d08ec2131f8f63e90e7f37d066e9c
michellekim2 <prebideul@gmail.com>
2023-11-22 17:18:27 -0500

    Added links

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,8 +38,7 @@ function Home(){
                 <Link to={'/'} href="https://vimeo.com/342333493" data-fancybox data-aos="fade-up" data-aos-delay="0" className="caption mb-4 d-inline-block">LabLink!</Link>
 
                 <h1 className="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100"><em>Research is to see what everybody else has seen, and to think what nobody else has thought.</em></h1>
-                <p className="mb-0" data-aos="fade-up" data-aos-delay="300"><a href="#" className="btn btn-secondary">Explore projects</a></p>
-
+                <p className="mb-0" data-aos="fade-up" data-aos-delay="300"><Link to="/projects" className="btn btn-secondary">Explore projects</Link></p>
               </div>
 
 
@@ -57,99 +56,99 @@ function Home(){
       <div className="container">
         <div className="row justify-content-center mb-3">
           <div className="col-lg-7 text-center" data-aos="fade-up" data-aos-delay="0">
-            <h2 className="line-bottom text-center mb-4">Research Areas</h2>
+            <h2 className="line-bottom text-center mb-4">What Research Areas are there in Computer Science?</h2>
           </div>
         </div>
         <div className="row align-items-stretch">
           <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="0">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Artificial_intelligence", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
               <div>
                 <i className="uil uil-atom"></i>
               </div>
               <div>
                 <h3>Artificial Intelligence</h3>
-                <span>1,391 projects</span>
+                <span></span>
               </div>
-            </Link>
+            </button>
+
           </div>
           <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="100">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
+          <button onClick={() => window.open("https://en.wikipedia.org/wiki/Data_mining", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
               <div>
                 <i className="uil uil-briefcase"></i>
               </div>
               <div>
-                <h3>Programming Languages</h3>
-                <span>3,234 projects</span>
+                <h3>Data Mining and Analytics</h3>
+                <span></span>
               </div>
-            </Link>
+            </button>
           </div>
           <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="200">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
-              <div>
-                <i className="uil uil-calculator"></i>
-              </div>
-              <div>
-                <h3>Scientific Computing Applications</h3>
-                <span>931 projects</span>
-              </div>
-            </Link>
-          </div>
-          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="300">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
-              <div>
-                <i className="uil uil-pen"></i>
-              </div>
-              <div>
-                <h3>Theory of Computation</h3>
-                <span>7,291 projects</span>
-              </div>
-            </Link>
-          </div>
-
-
-          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="0">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
-              <div>
-                <i className="uil uil-music"></i>
-              </div>
-              <div>
-                <h3>Computer Security</h3>
-                <span>9,114 projects</span>
-              </div>
-            </Link>
-          </div>
-          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="100">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Health_informatics", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
               <div>
                 <i className="uil uil-chart-pie"></i>
               </div>
               <div>
-                <h3>Databases and Data Mining</h3>
-                <span>2,391 projects</span>
+                <h3>Health Informatics</h3>
+                <span></span>
               </div>
-            </Link>
+            </button>
+          </div>
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Theory_of_computation", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
+              <div>
+                <i className="uil uil-calculator"></i>
+              </div>
+              <div>
+                <h3>Theory of Computation</h3>
+                <span></span>
+              </div>
+            </button>
+          </div>
+
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="0">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Scientific_Computer_Applications", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
+              <div>
+                <i className="uil uil-pen"></i>
+              </div>
+              <div>
+                <h3>Scientific Computer Applications</h3>
+                <span></span>
+              </div>
+            </button>
+          </div>
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="100">
+          <button onClick={() => window.open("https://en.wikipedia.org/wiki/Database", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
+              <div>
+                <i className="uil uil-atom"></i>
+              </div>
+              <div>
+                <h3>Database and Information Systems </h3>
+                <span></span>
+              </div>
+            </button>
           </div>
           <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="200">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
               <div>
                 <i className="uil uil-camera"></i>
               </div>
               <div>
-                <h3>Human-Computer Interaction</h3>
-                <span>7,991 projects</span>
+                <h3>Human Computer Interaction</h3>
+                <span></span>
               </div>
-            </Link>
+            </button>
           </div>
           <div className="col-sm-6 col-md-6 col-lg-3 mb-4" data-aos="fade-up" data-aos-delay="300">
-            <Link to={'/'} className="category d-flex align-items-start h-100">
+            <button onClick={() => window.open("https://en.wikipedia.org/wiki/Software_engineering", "_blank", "noopener,noreferrer")} className="category d-flex align-items-start h-100">
               <div>
                 <i className="uil uil-circle-layer"></i>
               </div>
               <div>
                 <h3>Software Engineering</h3>
-                <span>6,491 projects</span>
+                <span></span>
               </div>
-            </Link>
+            </button>
           </div>
 
 
@@ -157,7 +156,7 @@ function Home(){
 
         <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="400">
           <div className="col-lg-8 text-center">
-            <p>We have more categories here. <a href="#">Browse all</a></p>
+            <p>Learn more about specific projects at Emory <Link to="Projects">Browse all</Link></p>
           </div>
         </div>
       </div>
@@ -177,13 +176,13 @@ function Home(){
               <Link to={'/'}><img src="images/DDIHTS_cover.jpg" alt="Image" className="img-fluid"/></Link>
               <div className="custom-media-body">
                 <div className="d-flex justify-content-between pb-3">
-                  <div className="text-primary"><span className="uil uil-book-open"></span> <span>27 comments</span></div>
-                  <div className="review"><span className="icon-star"></span> <span>4.8</span></div>
+                  <div className="text-primary"><span className="uil uil-book-open"></span> <span></span></div>
+                  <div className="review"><span className=""></span> <span></span></div>
                 </div>
                 <h3>Data Driven Innovations for Human-Technology Symbiosis Lab</h3>
                 <p className="mb-4">NLP, AI, ML, HCI</p>
                 <div className="border-top d-flex justify-content-between pt-3 mt-3 align-items-center">
-                  <div><Link to={'/'}>Learn More</Link></div>
+                  <div><Link to={'Projects'}>Learn More</Link></div>
                 </div>
               </div>
             </div>
@@ -193,13 +192,13 @@ function Home(){
               <Link to={'/'}><img src="images/data_mining_cover.jpg" alt="Image" className="img-fluid"/></Link>
               <div className="custom-media-body">
                 <div className="d-flex justify-content-between pb-3">
-                  <div className="text-primary"><span className="uil uil-book-open"></span> <span>43 comments</span></div>
-                  <div className="review"><span className="icon-star"></span> <span>4.8</span></div>
+                  <div className="text-primary"><span className="uil uil-book-open"></span> <span></span></div>
+                  <div className="review"><span className=""></span> <span></span></div>
                 </div>
                 <h3>Practical Data Mining & Exploration Lab</h3>
                 <p className="mb-4">ML, Data Mining, Healthcare</p>
                 <div className="border-top d-flex justify-content-between pt-3 mt-3 align-items-center">
-                  <div><Link to={'/'}>Learn More</Link></div>
+                  <div><Link to={'Projects'}>Learn More</Link></div>
                 </div>
               </div>
             </div>
@@ -209,13 +208,13 @@ function Home(){
               <Link to={'/'}><img src="images/Simbiosys_cover.jpg" alt="Image" className="img-fluid"/></Link>
               <div className="custom-media-body">
                 <div className="d-flex justify-content-between pb-3">
-                  <div className="text-primary"><span className="uil uil-book-open"></span> <span>18 comments</span></div>
-                  <div className="review"><span className="icon-star"></span> <span>4.8</span></div>
+                  <div className="text-primary"><span className="uil uil-book-open"></span> <span></span></div>
+                  <div className="review"><span className=""></span> <span></span></div>
                 </div>
                 <h3>Simbiosys Lab</h3>
                 <p className="mb-4">Distributed Systems, Data Science</p>
                 <div className="border-top d-flex justify-content-between pt-3 mt-3 align-items-center">
-                  <div><Link to={'/'}>Learn More</Link></div>
+                  <div><Link to={'Projects'}>Learn More</Link></div>
                 </div>
               </div>
             </div>
@@ -322,7 +321,7 @@ function Home(){
             <img src="images/img-school-5-min.jpg" alt="image" className="img-fluid"/>
           </div>
           <div className="col-lg-7 ml-auto" data-aos="fade-up" data-aos-delay="100">
-            <h3 className="line-bottom mb-4">Why This Project</h3>
+            <h3 className="line-bottom mb-4">The Mission Behind LabLink!</h3>
             <p>Many reasons are as follows: </p>
 
             <div className="custom-accordion" id="accordion_1">
@@ -334,13 +333,10 @@ function Home(){
                 <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion_1">
                   <div className="accordion-body">
                     <div className="d-flex">
-                      <div className="accordion-img mr-4">
-                        <img src="images/img-school-1-min.jpg" alt="Image" className="img-fluid"/>
-                      </div>
                       <div>
-                        <p>One-stop shop for all Emory CS research information.</p>
-                        <p>Information about available research positions.</p>
-                        <p>Expansion potential to other departments. </p>
+                        <p>One-stop shop for all Emory CS research information</p>
+                        <p>Create an informed studybody and wide pool of applicants</p>
+                        <p>Find research related events in a website (vs email)</p>
                       </div>
                     </div>
                   </div>
@@ -349,17 +345,14 @@ function Home(){
 
               <div className="accordion-item">
                 <h2 className="mb-0">
-                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Current links to opportunities are scattered and not regularly updated.</button>
+                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">No Consolidated Recruitment Platform for Professors</button>
                 </h2>
                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion_1">
                   <div className="accordion-body">
                     <div className="d-flex">
-                      <div className="accordion-img mr-4">
-                        <img src="images/img-school-2-min.jpg" alt="Image" className="img-fluid"/>
-                      </div>
                       <div>
-                        <p>Social connectivity with current researchers. </p>
-                        <p>Collaboration potential with Emora chatbot for interactive information fetching.</p>
+                        <p>Students can self-nominate and take initative to raise their presence in the department</p>
+                        <p>Professors can post recruitment notices to gain student applications</p>
                       </div>
                     </div>
                   </div>
@@ -367,17 +360,15 @@ function Home(){
               </div>
               <div className="accordion-item">
                 <h2 className="mb-0">
-                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Listing of current Emory research events.</button>
+                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Need for CS community building</button>
                 </h2>
 
                 <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion_1">
                   <div className="accordion-body">
                     <div className="d-flex">
-                      <div className="accordion-img mr-4">
-                        <img src="images/img-school-3-min.jpg" alt="Image" className="img-fluid"/>
-                      </div>
                       <div>
-                        <p>Online announcements for ad-hoc meetings.</p>
+                        <p>Provide stable platform (vs. email) to access information on upcoming CS events</p>
+                        <p>Raise general awareness of what opportunities are available</p>
                       </div>
                     </div>
 
