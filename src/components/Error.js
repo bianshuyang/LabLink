1d5b52f8f85eacb83bc25a547a5f85046543c33e
scui24 <123330983+scui24@users.noreply.github.com>
2023-11-29 07:03:39 -0500

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -9,28 +9,40 @@ export default function Error() {
     setEditorHtml(html);
   }
 
+  // Custom toolbar options
+  const toolbarOptions = [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'font': [] }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+    ['link', 'image', 'video'],
+    ['emoji'],
+    ['clean']                                         // remove formatting
+  ];
+
   // The modules object is used to customize the toolbar options
   const modules = {
-    toolbar: [
-      [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-      [{ size: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' },
-      { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image', 'video'],
-      ['clean']
-    ],
+    toolbar: toolbarOptions,
     clipboard: {
       // Match visual, not literal, whitespace
       matchVisual: false,
-    }
+    },
+    // Include additional modules here
   };
 
   const formats = [
     'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'bold', 'italic', 'underline', 'strike',
     'list', 'bullet', 'indent',
-    'link', 'image', 'video'
+    'link', 'image', 'video',
+    'color', 'background', 'script',
+    'direction', 'emoji'
+    // Include additional formats here
   ];
 
   return (
@@ -40,7 +52,7 @@ export default function Error() {
         onChange={handleEditorChange}
         modules={modules}
         formats={formats}
-        theme="snow" // this prop is optional
+        theme="snow"
       />
     </div>
   );
